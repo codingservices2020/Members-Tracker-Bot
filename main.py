63f0700796cb8e228e4a8e9adc8f686854140b6b
codingservices2020 <codingservices2020@gmail.com>
2025-01-13 13:14:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from telegram.ext import (
 # load_dotenv()
 keep_alive()
 
-port = int(os.environ.get('PORT', 10000))  # Default to 10000 if no port is set
-app.run(host='0.0.0.0', port=port)
+# port = int(os.environ.get('PORT', 10000))  # Default to 10000 if no port is set
+# app.run(host='0.0.0.0', port=port)
 
 # Fetch bot token and allowed group ID from environment variables
 BOT_TOKEN = os.getenv('BOT_TOKEN')
